68cc223fdfd5df1d1942c02e673a19f381eccf86
zhuzhengping <49805682+goodzzp@users.noreply.github.com>
2019-07-12 09:54:08 +0800

    初始化

---
+++ doc/mybatis-generator.bat
@@ -0,0 +1,5 @@
+@echo off
+
+java -jar mybatis-generator-core-custom-1.3.7.jar -configfile recommend.xml -overwrite
+
+pause
---
+++ src/main/java/org/mybatis/generator/plugins/AddLimitOffsetPlugin.java
@@ -0,0 +1,130 @@
+package org.mybatis.generator.plugins;
+
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+
+import java.util.List;
+
+/*
+ * 功能描述：分页插件
+ * @author 朱正平
+ * @date 2015/6/13
+ * @param
+ * @return
+ */
+public class AddLimitOffsetPlugin  extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    /**
+     * 为每个Example类添加limit和offset属性已经set、get方法
+     */
+    @Override
+    public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+        PrimitiveTypeWrapper integerWrapper = FullyQualifiedJavaType.getIntInstance().getPrimitiveTypeWrapper();
+
+        Field limit = new Field();
+        limit.setName("limit");
+        limit.setVisibility(JavaVisibility.PRIVATE);
+        limit.setType(integerWrapper);
+        topLevelClass.addField(limit);
+
+        Method setLimit = new Method();
+        setLimit.setVisibility(JavaVisibility.PUBLIC);
+        setLimit.setName("setLimit");
+        setLimit.addParameter(new Parameter(integerWrapper, "limit"));
+        setLimit.addBodyLine("this.limit = limit;");
+        topLevelClass.addMethod(setLimit);
+
+        Method getLimit = new Method();
+        getLimit.setVisibility(JavaVisibility.PUBLIC);
+        getLimit.setReturnType(integerWrapper);
+        getLimit.setName("getLimit");
+        getLimit.addBodyLine("return limit;");
+        topLevelClass.addMethod(getLimit);
+
+        Field offset = new Field();
+        offset.setName("offset");
+        offset.setVisibility(JavaVisibility.PRIVATE);
+        offset.setType(integerWrapper);
+        topLevelClass.addField(offset);
+
+        Method setOffset = new Method();
+        setOffset.setVisibility(JavaVisibility.PUBLIC);
+        setOffset.setName("setOffset");
+        setOffset.addParameter(new Parameter(integerWrapper, "offset"));
+        setOffset.addBodyLine("this.offset = offset;");
+        topLevelClass.addMethod(setOffset);
+
+        Method getOffset = new Method();
+        getOffset.setVisibility(JavaVisibility.PUBLIC);
+        getOffset.setReturnType(integerWrapper);
+        getOffset.setName("getOffset");
+        getOffset.addBodyLine("return offset;");
+        topLevelClass.addMethod(getOffset);
+
+        return true;
+    }
+
+    /**
+     * 为Mapper.xml的selectByExample添加limit
+     */
+    @Override
+    public boolean sqlMapSelectByExampleWithoutBLOBsElementGenerated(XmlElement element,
+                                                                     IntrospectedTable introspectedTable) {
+
+        XmlElement ifLimitNotNullElement = new XmlElement("if");
+        ifLimitNotNullElement.addAttribute(new Attribute("test", "limit != null"));
+
+        XmlElement ifOffsetNotNullElement = new XmlElement("if");
+        ifOffsetNotNullElement.addAttribute(new Attribute("test", "offset != null"));
+        ifOffsetNotNullElement.addElement(new TextElement("limit ${offset}, ${limit}"));
+        ifLimitNotNullElement.addElement(ifOffsetNotNullElement);
+
+        XmlElement ifOffsetNullElement = new XmlElement("if");
+        ifOffsetNullElement.addAttribute(new Attribute("test", "offset == null"));
+        ifOffsetNullElement.addElement(new TextElement("limit ${limit}"));
+        ifLimitNotNullElement.addElement(ifOffsetNullElement);
+
+        element.addElement(ifLimitNotNullElement);
+
+        return true;
+    }
+
+    @Override
+    public boolean providerSelectByExampleWithBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return appendLimitForProvider(method);
+    }
+
+    @Override
+    public boolean providerSelectByExampleWithoutBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return appendLimitForProvider(method);
+    }
+
+    private boolean appendLimitForProvider(Method method) {
+        List<String> lines = method.getBodyLines();
+        lines.remove(lines.size() - 1);
+
+        // 添加limit语句到最后
+        String line = "String limit = \"\";\n" +
+                "        if (example != null && example.getLimit() != null) {\n" +
+                "            limit = \" limit \" + example.getLimit().toString();\n" +
+                "            if (example.getOffset() != null) {\n" +
+                "                limit = limit + \" offset \" + example.getOffset().toString();\n" +
+                "            }\n" +
+                "        }\n" +
+                "        return  sql.toString() + limit;";
+        lines.add(line);
+
+        return true;
+    }
+}
